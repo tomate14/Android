b807ae54e3c707b9ccecc97ce3bead3eba37a1be
juanfran93 <jfferreira1993@gmail.com>
2017-11-08 21:14:49 -0300

    maquetas filtro becas y ver perfil
    se crearon las maquetas de los filtros a becas y del perfil

--- app/src/main/java/ingenio/myapplication/MenuPrincipal.java
+++ app/src/main/java/ingenio/myapplication/MenuPrincipal.java
@@ -95,17 +95,22 @@ public class MenuPrincipal extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_buscarBecas) {
-            Intent mostrar = new Intent(MenuPrincipal.this,MostrarBecas.class);
-            mostrar.putExtra("listview", ID_VERBECAS);
-            startActivity(mostrar);
+            Intent filtro = new Intent(MenuPrincipal.this,FiltroBecasActivity.class);
+            startActivity(filtro);
+
         }
         if (id == R.id.nav_becasSugeridas) {
             Intent mostrar = new Intent(MenuPrincipal.this,MostrarBecas.class);
-            mostrar.putExtra("listview",ID_VERSUGERENCIAS);
+            mostrar.putExtra("listview", ID_VERSUGERENCIAS);
             startActivity(mostrar);
         }
         if (id == R.id.nav_editarDatos) {
 
+        }
+
+        if (id == R.id.nav_verPerfil){
+            Intent verPerfil = new Intent(MenuPrincipal.this,PerfilActivity.class);
+            startActivity(verPerfil);
         }
         if (id == R.id.nav_misBecas) {
             Intent mostrar = new Intent(MenuPrincipal.this,MostrarBecas.class);
