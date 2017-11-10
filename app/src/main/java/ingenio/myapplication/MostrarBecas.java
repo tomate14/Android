466f8659dc7ffd60a4d3a9ed331e254a0a748703
laudefe <30359546+laudefe@users.noreply.github.com>
2017-11-10 01:09:34 -0300

    -Se agrego la lupa en el action bar

--- app/src/main/java/ingenio/myapplication/MostrarBecas.java
+++ app/src/main/java/ingenio/myapplication/MostrarBecas.java
@@ -4,6 +4,8 @@ package ingenio.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ExpandableListView;
 
@@ -16,6 +18,7 @@ import Funcionalidad.Servicios;
 import entity.Beca;
 
 public class MostrarBecas extends AppCompatActivity {
+
 
     private ExpandableListView listView;
     private ListViewExtended mostrarInfo = null;
@@ -30,6 +33,7 @@ public class MostrarBecas extends AppCompatActivity {
         Intent intent = getIntent();
         int accion = intent.getIntExtra("listview",0);
         ArrayList<Beca> becas;
+
         switch (accion){
             case MenuPrincipal.ID_VERBECAS:
                 //Armar un vector de becas
@@ -59,7 +63,35 @@ public class MostrarBecas extends AppCompatActivity {
             case android.R.id.home:
                 onBackPressed();
                 return true;
+            case R.id.action_search:
+                openSearch();
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openSearch() {
+        Intent filtro = new Intent(MostrarBecas.this,FiltroBecasActivity.class);
+        startActivity(filtro);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        Intent intent = getIntent();
+        int accion = intent.getIntExtra("listview",0);
+        MenuItem register = menu.findItem(R.id.action_search);
+        if (accion == MenuPrincipal.ID_VERBECAS){
+            register.setVisible(true);
+        }
+
+        return true;
     }
 }
