adc07c31b61c70f814038bb7d0b99dd9e539cb1c
Adrian Quevedo <adrianqvd@gmail.com>
2017-12-11 17:35:57 -0300

    minorFix Sugeridas

--- app/src/main/java/ingenio/myapplication/MostrarBecas.java
+++ app/src/main/java/ingenio/myapplication/MostrarBecas.java
@@ -88,7 +88,7 @@ public class MostrarBecas extends AppCompatActivity implements
                 break;
             case MenuPrincipal.ID_VERSUGERENCIAS:
                 setTitle(getString(R.string.activity_verbecas_sugeridas));
-                params = MenuPrincipal.OPERACION_VERBECASSUGERENCIAS+"/"+String.valueOf(MenuPrincipal.user.getIdTipoEstudiante());
+                params = MenuPrincipal.OPERACION_VERBECASSUGERENCIAS+"/"+String.valueOf(MenuPrincipal.user.getIdTipoEstudiante()+"/"+String.valueOf(MenuPrincipal.user.getIdusuario()));
                 break;
             case MenuPrincipal.ID_VERBECASINTERES:
                 setTitle(getString(R.string.activity_verbecas_interes));
