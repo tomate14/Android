fd7dc75f472317ea1e0cb7f524b89871637e66b4
Maxi <maxiroselli@gmail.com>
2017-12-11 19:26:48 -0300

    Servicio Notificacion
    asd

--- app/src/main/java/adapters/ListViewNotificaciones.java
+++ app/src/main/java/adapters/ListViewNotificaciones.java
@@ -1,10 +1,14 @@
 package adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import entity.Notificacion;
@@ -38,12 +42,16 @@ public class ListViewNotificaciones extends ListViewExtended {
 
         View inflate = View.inflate(contexto, R.layout.infonotificacion,null);
 
-        txtTitulo  = (TextView) inflate.findViewById(R.id.txtTitulo);
-        txtFecha = (TextView) inflate.findViewById(R.id.txtFecha);
+        SimpleDateFormat mdyFormat = new SimpleDateFormat("EEEE, dd MMMM yyyy");
+
         txtEntidad = (TextView) inflate.findViewById(R.id.txtEntidad);
-        txtEntidad.setText(anuncios.get(groupPosition).getEntidad());
+        txtEntidad.setText(anuncios.get(groupPosition).getNombreEntidad());
+
+        txtTitulo  = (TextView) inflate.findViewById(R.id.txtTitulo);
         txtTitulo.setText(anuncios.get(groupPosition).getTitulo());
-        txtFecha.setText(anuncios.get(groupPosition).getFecha().toString());
+
+        txtFecha = (TextView) inflate.findViewById(R.id.txtFecha);
+        txtFecha.setText(mdyFormat.format(anuncios.get(groupPosition).getFecha()));
 
         return inflate;
     }
@@ -52,11 +60,63 @@ public class ListViewNotificaciones extends ListViewExtended {
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
         TextView tv = new TextView(contexto);
         TextView txtTexto;
+        final int posicion = groupPosition;
+
+        if (convertView == null) {
+            convertView = View.inflate(contexto, R.layout.infobecashijo, null);
+
+
+            final Button button = (Button) convertView.findViewById(R.id.btnTelefono);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(android.content.Intent.ACTION_DIAL, Uri.parse("tel:"+becas.get(posicion).getTelefono()));
+                    contexto.startActivity(intent);
+                }
+            });
+
+            final Button button2 = (Button) convertView.findViewById(R.id.btnInfo);
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openChrome(anuncios.get(posicion).getWeb());
+                }
+            });
+
+
+        }
+
         View inflate = View.inflate(contexto, R.layout.infobecashijo,null);
+
+        TextView textView4;
+        textView4 = (TextView) inflate.findViewById(R.id.textView4);
+        textView4.setVisibility(View.INVISIBLE);
+
+        TextView txtFechaFin;
+        txtFechaFin = (TextView) inflate.findViewById(R.id.txtFechaFin);
+        txtFechaFin.setVisibility(View.INVISIBLE);
+
+        TextView textView3;
+        textView3 = (TextView) inflate.findViewById(R.id.textView3);
+        textView3.setVisibility(View.INVISIBLE);
+
+        TextView txtFechaIni;
+        txtFechaIni = (TextView) inflate.findViewById(R.id.txtFechaIni);
+        txtFechaIni.setVisibility(View.INVISIBLE);
+
         txtTexto  = (TextView) inflate.findViewById(R.id.txtTexto);
         txtTexto.setText(anuncios.get(groupPosition).getInformacion());
         tv.setTextSize(12);
         return inflate;
+    }
+
+    void openChrome(String url) {
+        Uri uri = Uri.parse("googlechrome://navigate?url=" + url);
+        Intent i = new Intent(Intent.ACTION_VIEW, uri);
+        if (i.resolveActivity(contexto.getPackageManager()) == null) {
+            i.setData(Uri.parse(url));
+        }
+        contexto.startActivity(i);
     }
 
 }
